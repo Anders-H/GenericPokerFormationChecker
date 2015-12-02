15246ba54cf685237a96b96314d94dcf15c614a0
Anders-H <anders@winsoft.se>
2015-12-02 11:36:34 +0100

    Project files.

---
+++ TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Winsoft.Gaming.GenericPokerFormationChecker;
+
+namespace TestConsole
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         do
+         {
+            Console.Write("Hand> ");
+            var hand = Console.ReadLine().Trim();
+            if (hand == "")
+               return;
+         } while (true);
+      }
+   }
+}
