94f40b381ccd271e7d51feb775e31790e879c47b
Anders-H <anders@winsoft.se>
2015-12-15 17:58:17 +0100

    DeckManager (used for cheating with AI) finished.

--- Winsoft.Gaming.GenericPokerFormationChecker/DeckManager.cs
+++ Winsoft.Gaming.GenericPokerFormationChecker/DeckManager.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Winsoft.Gaming.GenericPokerFormationChecker
 {
+    /// <summary>
+    ///     Class designed for a cheating computer player in a poker game.
+    /// </summary>
     public class DeckManager
     {
         private Deck deck { get; set; }
@@ -31,11 +34,59 @@ namespace Winsoft.Gaming.GenericPokerFormationChecker
             return s.ToString();
         }
 
+
+        private class NewDeckStructure
+        {
+            private int highestDeck2Score = -1;
+            internal string Hand1 { get; set; }
+            internal List<Tuple<string, int>> Hands2 { get; set; } = new List<Tuple<string, int>>();
+            internal Deck Deck { get; set; }
+            internal int HighestDeck2Score
+            {
+                get
+                {
+                    if (highestDeck2Score < 0)
+                    {
+                        var s = 0;
+                        Hands2.ForEach(x => { if (x.Item2 > s) s = x.Item2; });
+                        highestDeck2Score = s;
+                    }
+                    return highestDeck2Score;
+                }
+            }
+            public string HighestHand2() => Hands2.Where(x => x.Item2 == HighestDeck2Score).First().Item1;
+        }
+
+        /// <summary>
+        ///     For a cheating computer player in a poker game, creates a used deck with enough card for one player to make one swap.
+        /// </summary>
+        /// <param name="secondHandQuality">Cheat level.</param>
+        /// <returns></returns>
         public Tuple<string, string> PopHands(int secondHandQuality)
         {
             var deckCount = secondHandQuality > 0 ? (int)(secondHandQuality / 8) : 0;
             var redealCount = secondHandQuality % 8;
-            return Tuple.Create(PopHand(), PopHand());
+            var structures = new List<NewDeckStructure>();
+            for (int i = 0; i < deckCount + 1; i++)
+            {
+                var structure = new NewDeckStructure();
+                structure.Deck = new Deck();
+                structure.Deck.Shuffle();
+                structure.Hand1 = $"{structure.Deck.Pop()},{structure.Deck.Pop()},{structure.Deck.Pop()},{structure.Deck.Pop()},{structure.Deck.Pop()}";
+                var redeals = (i < deckCount ? 8 : redealCount + 1);
+                for (int j = 0; j < redeals; j++)
+                {
+                    var hand2 = $"{structure.Deck.Pop()},{structure.Deck.Pop()},{structure.Deck.Pop()},{structure.Deck.Pop()},{structure.Deck.Pop()}";
+                    var fc = new FormationChecker(hand2);
+                    fc.CheckFormation();
+                    var hand2score = fc.Score;
+                    structure.Hands2.Add(Tuple.Create(hand2, hand2score));
+                }
+                structures.Add(structure);
+            }
+            structures.Sort((a, b) => (a.HighestDeck2Score > b.HighestDeck2Score ? 1 : (a.HighestDeck2Score < b.HighestDeck2Score ? -1 : 0)));
+            this.deck = structures.Last().Deck;
+            return Tuple.Create(structures.Last().Hand1, structures.Last().HighestHand2());
         }
 
         public bool CanPopHand { get { return Count >= 5; } }
