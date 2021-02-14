3f6071a87ff1f6b63e43d532a180708d057e89a2
Anders-H <anders@winsoft.se>
2021-02-14 11:42:20 +0100

    Card parser covered by tests.

---
+++ GenericPokerFormationCheckerTests/CardListTests.cs
@@ -0,0 +1,24 @@
+﻿using Winsoft.Gaming.GenericPokerFormationChecker;
+using Xunit;
+
+namespace GenericPokerFormationCheckerTests
+{
+    public class CardListTests
+    {
+        [Fact]
+        public void CanCopyList()
+        {
+            var cards = new Card[]
+            {
+                Card.Parse("CLB06"),
+                Card.Parse("SPD03"),
+                Card.Parse("HRT06")
+            };
+            var cardList = new CardList(cards);
+            Assert.True(cardList.Count == 3);
+            Assert.True(cardList[0].ToString() == "CLB06");
+            Assert.True(cardList[1].ToString() == "SPD03");
+            Assert.True(cardList[2].ToString() == "HRT06");
+        }
+    }
+}
